1dbc9aa52cc1639edcd1003671ab9b86d92d169e
azhuravel <zhuravel.msk@gmail.com>
2022-03-20 22:11:13 +0300

    comments

--- src/dwitter_assets/src/services/wealthService.js
+++ src/dwitter_assets/src/services/wealthService.js
@@ -1,4 +1,10 @@
+// RosettaApi is an open-source specification for integrating with blockchain
+// About RosettaApi: https://www.rosetta-api.org/docs/welcome.html
+// About RosettaApi for Dfinity: https://smartcontracts.org/docs/integration/ledger-quick-start.html
+//
+// The implemenation in rosettaApi.js is really copy paste from https://www.stoicwallet.com/
 import RosettaApi from './rosettaApi.js';
+
 import { principalToAccountIdentifier } from '../services/utils.js';
 
 const rosettaApi = new RosettaApi();
